07f099c0b7a1d87f3a49c2e88f3ff673a56506f0
black <black@example.com>
2026-07-15 23:48:12 +0500

    Add greetBlack function without calling it

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def say_hello():
-    print("hello everyone")
+    print("hello")
 
 def greetWhite():
     print("hello white")
+
+def greetBlack():
+    print("hello black")
 
 if __name__ == "__main__":
     say_hello()
